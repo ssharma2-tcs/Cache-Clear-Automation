7d16e723da803ee95410087c284b3c200058cc6e
Sourav Sharma <ssharma2@Tapestry.com>
2025-02-26 15:56:00 -0300

    Latest code working fine

--- cpCodeCache.js
+++ cpCodeCache.js
@@ -17,17 +17,19 @@ require("dotenv").config();
     await driver.sleep(3000);
     
     // staging
-    let sfccCode = ['1329410', '1329415', '1329811'];
-    let pwaCode = ['1329411', '1329419', '1329810'];
+    // let sfccCode = ['1329410', '1329415', '1329811'];
+    // let pwaCode = ['1329411', '1329419', '1329810'];
     
     //production
-    // let sfccCode = ['1338995', '1338998', '1339116'];
-    // let pwaCode = ['1338996', '1338999', '1339114'];
+    let sfccCode = ['1338995', '1338998', '1339116'];
+    let pwaCode = ['1338996', '1338999', '1339114'];
 
     await driver.get("https://control.akamai.com/apps/fast-purge/#/");
     // await driver.get("chrome://newtab");
-    await driver.sleep(5000);
-    await driver.findElement(By.css("label[for='akam-radio-3-input']")).click();
+    await driver.sleep(3000);
+    await driver.wait(
+      until.elementLocated(By.css("label[for='akam-radio-3-input']")),
+      10000).click();
     await driver.sleep(2000);
     
     for (let i = 0; i < 6; i++) {
@@ -40,36 +42,28 @@ require("dotenv").config();
     }
 
     async function cPCodeCacheClear(c, i) {
-      let code = await driver.findElement(
-        By.css(`input[placeholder="Select CP codes (max 100)"]`)
-      );
-      
+      let code = await driver.wait(until.elementLocated(By.css(`input[placeholder="Select CP codes (max 100)"]`)), 10000)
       await code.click();
-      await driver.sleep(2000);
       await code.clear();
       await code.sendKeys(c);
-      await driver.sleep(2000);
-      await driver.findElement(By.className("core-2-0 pulsar")).click();
+      // await driver.sleep(2000);
+      // await driver.findElement(By.className("core-2-0 pulsar")).click();
       await driver.sleep(3000);
-      let selectTd = await driver.findElement(By.css(`td[class="cdk-cell cdk-column-cpCode akam-cell akam-column-cpCode ng-star-inserted"]`));
+      let selectTd = await driver.wait(until.elementLocated(By.css(`td[class="cdk-cell cdk-column-cpCode akam-cell akam-column-cpCode ng-star-inserted"]`)), 10000)
       let seletedCode = (await selectTd.getText()).trim();
       
       if(seletedCode.includes(c)){
         selectTd.click();
       }
       await driver.sleep(2000);
-      let selectEnv = await driver.findElement(
-        By.css(`span[translate="template.ccu.networkType.prod"]`)
-      );
+      let selectEnv = await driver.wait(until.elementLocated(By.css(`span[translate="template.ccu.networkType.prod"]`)), 10000)
       await selectEnv.click();
       await driver.sleep(2000);
-      let selectCacheway = await driver.findElement(
-        By.css(`span[translate="template.ccu.refreshType.purge"]`)
-      );
+      let selectCacheway = await driver.wait(until.elementLocated(By.css(`span[translate="template.ccu.refreshType.purge"]`)), 10000)
       await selectCacheway.click();
       await driver.sleep(3000);
-      let cacheIt = await driver.findElement(
-        By.css(`button[translate="template.ccu.button.submit"]`)
+      let cacheIt = await driver.wait(
+          until.elementLocated(By.css(`button[translate="template.ccu.button.submit"]`)), 10000
       );
       await cacheIt.click();
       await driver.sleep(3000);
--- index.js
+++ index.js
@@ -1,47 +1,53 @@
 const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
-require('dotenv').config();
+require("dotenv").config();
 
 (async function example() {
-  let options = new chrome.Options();
-  // options.options_["debuggerAddress"] = "localhost:9222";
+  // let options = new chrome.Options();
+  // // options.options_["debuggerAddress"] = "localhost:9222";
 
-  options.addArguments(
-    "user-data-dir=C:/Users/ssharma2/AppData/Local/Google/Chrome/User Data"
-  );
-  options.addArguments("profile-directory=Default");
-  options.excludeSwitches(["enable-automation"]);
-  // // options.addArguments("load-extension=C:/Users/ssharma2/AppData/Local/Google/Chrome/User Data/Default/Extensions/bhghoamapcdpbohphigoooaddinpkbai/8.0.1_0/")
+  // options.addArguments(
+  //   "user-data-dir=C:/Users/ssharma2/AppData/Local/Google/Chrome/User Data"
+  // );
+  // options.addArguments("profile-directory=Default");
+  // options.excludeSwitches(["enable-automation"]);
+  // // // options.addArguments("load-extension=C:/Users/ssharma2/AppData/Local/Google/Chrome/User Data/Default/Extensions/bhghoamapcdpbohphigoooaddinpkbai/8.0.1_0/")
+  // let driver = await new Builder()
+  //   .forBrowser("chrome")
+  //   .setChromeOptions(options)
+  //   .build();
+
+  let options = new chrome.Options();
+  options.options_["debuggerAddress"] = "localhost:9222";
+
   let driver = await new Builder()
     .forBrowser("chrome")
     .setChromeOptions(options)
     .build();
+
   try {
     await driver.manage().window().maximize();
-    // let tabs = await driver.getAllWindowHandles();
-    // console.log("open tab", tabs);
-    // await driver.switchTo().window(tabs[0]);
+
+    // await driver.get("https://control.akamai.com/apps/auth/");
+    // await driver.sleep(5000);
+    // let username = await driver.findElement(By.css("input[name='username']"));
+    // await username.sendKeys(`${process.env.EMAIL}`);
+    // await driver.sleep(2000);
+    // await driver.findElement(By.css("button[name='next-btn']")).click();
+    // await driver.sleep(2000);
+    // let pass = await driver.findElement(By.css("input[type='password']"));
+    // await pass.sendKeys(`${process.env.PASSWORD}`, Key.RETURN);
+    // await driver.sleep(3000);
+    // let otp = await driver.findElement(By.css("input[name='otp-code-0']"));
+    // await otp.click();
+    // await driver.sleep(10000);
+    // let submit = await driver.findElement(
+    //   By.css("button[translate='auth.verificationForm.doneBtnText']")
+    // );
+    // await submit.click();
+    // await driver.sleep(7000);
     
-    await driver.get("https://control.akamai.com/apps/auth/");
-    await driver.sleep(5000);
-    let username = await driver.findElement(By.css("input[name='username']"));
-    await username.sendKeys(`${process.env.EMAIL}`);
-    await driver.sleep(2000);
-    await driver.findElement(By.css("button[name='next-btn']")).click();
-    await driver.sleep(2000);
-    let pass = await driver.findElement(By.css("input[type='password']"));
-    await pass.sendKeys(`${process.env.PASSWORD}`, Key.RETURN);
-    await driver.sleep(3000);
-    let otp = await driver.findElement(By.css("input[name='otp-code-0']"));
-    await otp.click();
-    await driver.sleep(10000);
-    let submit = await driver.findElement(
-      By.css("button[translate='auth.verificationForm.doneBtnText']")
-    );
-    await submit.click();
-    await driver.sleep(7000);
     await driver.get("https://control.akamai.com/apps/fast-purge/#/");
-    await driver.sleep(3000);
     let cachetags = [
       "coachRetailJP_prod_global",
       "coachRetailJP_prod_home",
@@ -60,33 +66,34 @@ require('dotenv').config();
       "katespadeJP_prod_category",
       "katespadeJP_prod_product",
       "katespadeJP_prod_search",
-      "katespadeJP_prod_content"
+      "katespadeJP_prod_content",
     ];
-    
-    /*let cachetags = [
-      "coachRetailJP_stage_global",
-      "coachRetailJP_stage_home",
-      "coachRetailJP_stage_category",
-      "coachRetailJP_stage_product",
-      "coachRetailJP_stage_search",
-      "coachRetailJP_stage_content"
-    ];*/
-    
+
+    // let cachetags = [
+    //   "coachRetailJP_stage_global",
+    //   "coachRetailJP_stage_home",
+    //   "coachRetailJP_stage_category",
+    //   "coachRetailJP_stage_product",
+    //   "coachRetailJP_stage_search",
+    //   "coachRetailJP_stage_content"
+    // ];
+
     for (let i = 0; i < cachetags.length; i++) {
+      await driver.sleep(3000);
       await driver
         .findElement(By.css("label[for='akam-radio-4-input']"))
         .click();
       await driver.sleep(3000);
-      let input = await driver.findElement(
-        By.css(
-          `input[placeholder="Type a valid cache tag, and press Enter or Tab after each tag entered"]`
-        )
+      let input = await driver.wait(until.elementLocated(By.css(
+        `input[placeholder="Type a valid cache tag, and press Enter or Tab after each tag entered"]`
+      ))
+       ,10000 
       );
       await input.click();
       await input.sendKeys(cachetags[i]);
       await driver.sleep(2000);
       await driver.findElement(By.className("core-2-0 pulsar")).click();
-      
+
       await driver.sleep(2000);
       let selectEnv = await driver.findElement(
         By.css(`span[translate="template.ccu.networkType.prod"]`)
@@ -101,9 +108,9 @@ require('dotenv').config();
       let cacheIt = await driver.findElement(
         By.css(`button[translate="template.ccu.button.submit"]`)
       );
-      
+
       await cacheIt.click();
-      console.log("Cache is cleared for ", cachetags[i]);
+      console.log("Cache is cleared for ",cachetags[i]);
       await driver.sleep(4000);
     }
 
